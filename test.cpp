8c0d0a54d8a10e04dec7acbf38a63209ffddc848
Chris <1020158376@qq.com>
2022-02-28 22:41:30 +0800

    fix some wrongs

--- test.cpp
+++ test.cpp
@@ -26,15 +26,9 @@ enum Event
 };
 
 // function to test state transfer
-void Func_1to1(){cout << "from state 1 to state 1" << endl;}
-void Func_1to2(){cout << "from state 1 to state 2" << endl;}
-void Func_1to3(){cout << "from state 1 to state 3" << endl;}
-void Func_2to1(){cout << "from state 2 to state 1" << endl;}
-void Func_2to2(){cout << "from state 2 to state 2" << endl;}
-void Func_2to3(){cout << "from state 2 to state 3" << endl;}
-void Func_3to1(){cout << "from state 3 to state 1" << endl;}
-void Func_3to2(){cout << "from state 3 to state 2" << endl;}
-void Func_3to3(){cout << "from state 3 to state 3" << endl;}
+void Func_1(){cout << "Current state is 1" << endl;}
+void Func_2(){cout << "Current state is 2" << endl;}
+void Func_3(){cout << "Current state is 3" << endl;}
 
 // main 
 int main()
@@ -42,32 +36,32 @@ int main()
     // create new FSM and init state
     easyFSM<State,Event> FSM(state1);
     // bind function to FSM
-    FSM.bind(state1, state1, E1to1, Func_1to1);
-    FSM.bind(state1, state2, E1to2, Func_1to2);
-    FSM.bind(state1, state3, E1to3, Func_1to3);
-    FSM.bind(state2, state1, E2to1, Func_2to1);
-    FSM.bind(state2, state2, E2to2, Func_2to2);
-    FSM.bind(state2, state3, E2to3, Func_2to3);
-    FSM.bind(state3, state1, E3to1, Func_3to1);
-    FSM.bind(state3, state2, E3to2, Func_3to2);
-    FSM.bind(state3, state3, E3to3, Func_3to3);
+    FSM.bind(state1, state1, E1to1, Func_1);
+    FSM.bind(state1, state2, E1to2, Func_2);
+    FSM.bind(state1, state3, E1to3, Func_3);
+    FSM.bind(state2, state1, E2to1, Func_1);
+    FSM.bind(state2, state2, E2to2, Func_2);
+    FSM.bind(state2, state3, E2to3, Func_3);
+    FSM.bind(state3, state1, E3to1, Func_1);
+    FSM.bind(state3, state2, E3to2, Func_2);
+    FSM.bind(state3, state3, E3to3, Func_3);
     // start test
     cout << "start testing..." << endl; 
     // print current state
-    cout << "current state is " << FSM.CurState() << endl;
+    cout << "Current state is " << FSM.CurState() << endl;
     // 1-->3-->1-->2-->3-->2-->1
+    cout << "1-->3" << endl;
     FSM.handleEvent(E1to3);
-    cout << "current state is " << FSM.CurState() << endl;
+    cout << "3-->1" << endl;
     FSM.handleEvent(E3to1);
-    cout << "current state is " << FSM.CurState() << endl;
+    cout << "1-->2" << endl;
     FSM.handleEvent(E1to2);
-    cout << "current state is " << FSM.CurState() << endl;
+    cout << "2-->3" << endl;
     FSM.handleEvent(E2to3);
-    cout << "current state is " << FSM.CurState() << endl;
+    cout << "3-->2" << endl;
     FSM.handleEvent(E3to2);
-    cout << "current state is " << FSM.CurState() << endl;
+    cout << "2-->1" << endl;
     FSM.handleEvent(E2to1);
-    cout << "current state is " << FSM.CurState() << endl;
     // end test
     cout << "end test" << endl;
     return 0;
